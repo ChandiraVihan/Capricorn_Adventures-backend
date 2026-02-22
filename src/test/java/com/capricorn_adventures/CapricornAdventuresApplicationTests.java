8b59e4c908914d18f6bfb0bf26bd262accdf6749
ChandiraVihan <chandiravihan@gmail.com>
2026-02-22 19:17:49 +0530

    spring-init

---
+++ src/test/java/com/capricorn_adventures/CapricornAdventuresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capricorn_adventures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapricornAdventuresApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
