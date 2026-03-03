b2ac9c7ccbd3cbe9db57267afcded2789c42fdb7
Induranga FBD <83300979+FBID10@users.noreply.github.com>
2026-03-03 14:44:03 +0000

    feat: add repository interfaces for entities

---
+++ src/main/java/com/capricorn_adventures/repository/RoomImageRepository.java
@@ -0,0 +1,9 @@
+package com.capricorn_adventures.repository;
+
+import com.capricorn_adventures.entity.RoomImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoomImageRepository extends JpaRepository<RoomImage, Long> {
+}
