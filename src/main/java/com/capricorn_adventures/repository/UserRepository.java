6a0dcda9fc4269ee92538098bc6ee860aa0f422c
Chandira W. D. V. <chandiravihan@gmail.com>
2026-04-23 22:17:20 +0530

    missing import

--- src/main/java/com/capricorn_adventures/repository/UserRepository.java
+++ src/main/java/com/capricorn_adventures/repository/UserRepository.java
@@ -4,6 +4,7 @@ import com.capricorn_adventures.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByEmail(String email);
